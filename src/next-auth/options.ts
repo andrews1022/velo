d02c8b160559e0372291d11657ddde22f52de978
Andrew Shearer <andrewshearerdev@gmail.com>
2023-12-13 19:27:58 -0800

    updated next auth options
    - added google provider
    - added database as session strategy
    - added boilerplate code for sign in callback

--- src/next-auth/options.ts
+++ src/next-auth/options.ts
@@ -1,7 +1,28 @@
-import type { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const options: AuthOptions = {
-  providers: []
+import type { NextAuthOptions } from "next-auth";
+
+const options: NextAuthOptions = {
+  callbacks: {
+    async signIn({ account, credentials, email, profile, user }) {
+      console.log("account", account);
+      console.log("credentials", credentials);
+      console.log("email", email);
+      console.log("profile", profile);
+      console.log("user", user);
+
+      return true;
+    }
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ],
+  session: {
+    strategy: "database"
+  }
 };
 
 export { options };

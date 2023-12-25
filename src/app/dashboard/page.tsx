a84669c20fab21cdef9c40008f0ccc591207af19
Andrew Shearer <andrewshearerdev@gmail.com>
2023-12-25 12:07:41 -0800

    updated dashboard page with session/auth protection

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,20 @@
-const DashboardPage = () => {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+import { options } from "@/next-auth/options";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div>
       <h1>DashboardPage</h1>
+
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 };
